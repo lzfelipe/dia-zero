d42a8e769d76320cfa3dfb0bb28a6aaea46ed0b2
Luiz Felipe Veiga <lzfelipevs@gmail.com>
2021-11-25 20:34:41 -0300

    Ajustes menus

--- pages/index.js
+++ pages/index.js
@@ -52,18 +52,18 @@ export default function Home() {
           </h1>
         </div>
 
-        <div className="col-12 d-flex justify-content-between">
-          <a href="#choices-2">
+        <div className="col-12 row  justify-content-between">
+          <a href="#choices-2" className="col-12 col-md-4">
             <h1 className="choices-text">
               SIMM<span className="text-orange">!</span>
             </h1>
           </a>
-          <a href="#choices-2">
+          <a href="#choices-2" className="col-12 col-md-4">
             <h1 className="choices-text">
               AH VEJO, JÁ TO AQUI<span className="text-orange">...</span>
             </h1>
           </a>
-          <a href="#inicio">
+          <a href="#inicio" className="col-12 col-md-4">
             <h1 className="choices-text">
               NÃO, VALEU<span className="text-orange">.</span>
             </h1>
@@ -80,33 +80,35 @@ export default function Home() {
           <img src="images/logo.png" />
         </div>
 
-        <div className="col-12 ">
+        <div className="col-12 pt-4">
           <h1 className="secondary-title">
             QUER VER UM POUCO POR TRÁS DO PROJETO
             <span className="text-orange">?</span>
           </h1>
         </div>
 
-        <div className="col-12 d-flex justify-content-between">
-          <a href="#rascunhos">
+        <div className="col-12 row justify-content-between pt-4">
+          <a href="#rascunhos" className="col-12 col-md-4">
             <h1 className="choices-text text-center">RASCUNHOS</h1>
           </a>
-          <a href="#integrantes">
+          <a href="#integrantes" className="col-12 col-md-4">
             <h1 className="choices-text text-center">INTEGRANTES</h1>
           </a>
           <a
             href="https://docs.google.com/document/d/1XqEPoOnPYqJdhLekd_7WRXYl2Qn6fp6b/edit?usp=sharing&ouid=101128919413065920640&rtpof=true&sd=true"
             target="_blank"
             rel="noreferrer"
+            className="col-12 col-md-4"
           >
             <h1 className="choices-text text-center">MONOGRAFIA</h1>
           </a>
         </div>
-        <div className="col-12 d-flex justify-content-between">
+        <div className="col-12 row d-flex justify-content-between pb-4">
           <a
             href="https://drive.google.com/file/d/14kySOVemQuiiCt0KKWgc1VLpDBb4z1qb/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
+            className="col-12 col-md-4"
           >
             <h1 className="choices-text text-center"> BIBLÍA</h1>
           </a>
@@ -114,6 +116,7 @@ export default function Home() {
             href="https://greeneletron.org.br/localizador"
             target="_blank"
             rel="noreferrer"
+            className="col-12 col-md-4"
           >
             <h1 className="choices-text text-center">RECICLE</h1>
           </a>
@@ -121,6 +124,7 @@ export default function Home() {
             href="https://drive.google.com/file/d/1QlUZqBFEJVNEd_b_AlmxAuQGXOIWXzWn/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
+            className="col-12 col-md-4"
           >
             <h1 className="choices-text text-center">DESIGN KIT</h1>
           </a>
