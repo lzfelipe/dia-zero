727c77fcb3b27c22859681863eda568db19ca47d
Luiz Felipe Veiga <lzfelipevs@gmail.com>
2021-11-28 20:19:40 -0300

    links e textos videos

--- components/Video.js
+++ components/Video.js
@@ -1,16 +1,34 @@
-
 export default function Video(props) {
-    return (
+  return (
     <div className="container section d-flex justify-content-center align-items-center text-center py-5">
-        <div className="">
-          <h1 className="main-title mb-5">{props.title}</h1>
-          <iframe src="https://www.youtube.com/embed/gVuF0tziRZQ" style={{width: '70vw', height: '40vw'}} />  
-          <h2 className="my-5">Esse vídeo tem apenas 3 minutos ou alguns anos?</h2>  
-          <p>Quer saber um pouco mais?</p> 
-          <button className="btn btn-video">CLIQUE AQUI</button>
-          
-        </div>
+      <div className="">
+        <h1 className="main-title mb-5">{props.title}</h1>
+        {props.title == "PILOTO" ? (
+          <iframe
+            src="https://www.youtube.com/embed/gVuF0tziRZQ"
+            style={{ width: "70vw", height: "40vw" }}
+          />
+        ) : (
+          <h1 className="main-title-name text-orange">EM BREVE</h1>
+        )}
+
+        {props.title == "PILOTO" ? (
+          <>
+            <h2 className="my-5">
+              Esse vídeo tem apenas 3 minutos ou alguns anos?
+            </h2>
+            <p>Quer saber um pouco mais?</p>
+            <a
+              className="btn btn-video"
+              href="https://brasil.elpais.com/brasil/2018/11/09/tecnologia/1541771036_210342.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              CLIQUE AQUI
+            </a>
+          </>
+        ) : null}
+      </div>
     </div>
-    )
-  }
-  
+  );
+}
